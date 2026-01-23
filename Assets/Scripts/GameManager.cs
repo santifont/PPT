3a7a09bc678efd6b2f03d981eb16a74816511399
santifont <sanalffer@alu.edu.gva.es>
2026-01-23 16:27:26 +0100

    Pequeños cambios

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,9 +11,10 @@ public class GameManager : MonoBehaviour
 
     // DATOS DE LA PARTIDA
     int ronda = 1;
-    int victoriasJugador = 0;
+    int maxRondas = 5;
+    int puntosJugador = 0;
     int empates = 0;
-    int victoriasIA = 0;
+    int puntosIA = 0;
 
     // DATOS DE LA RONDA
     int tiradaJugador;
@@ -37,15 +38,20 @@ public class GameManager : MonoBehaviour
     {
         if (turno == 0) // TIRADA JUGADOR
         {
-
+            
         }
-        else // TIRADA IA
+        else if (turno == 1) // TIRADA IA
         {
             TiradaIA();
-
             //  CALCULAR QUIEN HA GANADO
             CalculoGanador();
             turno = 0;
+            ronda++;
+        }
+
+        if (ronda == maxRondas)
+        {
+            StartCoroutine(FinDeLaPartida());
         }
     }
 
@@ -56,7 +62,7 @@ public class GameManager : MonoBehaviour
     public void TiradaJugador(int tirada) 
     {
         tiradaJugador = tirada;
-        turno = 1;
+        StartCoroutine(AvanzaRonda());
     }
 
     public void TiradaIA()
@@ -69,14 +75,17 @@ public class GameManager : MonoBehaviour
        if (tiradaJugador == tiradaIA)
         {
             empates++;
+            StartCoroutine(VictoriaJugador());
         }
         else if (tiradaJugador == 0 & tiradaIA == 2 || tiradaJugador == 1 && tiradaIA == 0 || tiradaJugador == 2 && tiradaIA == 1)
         {
-            victoriasJugador++;
+            puntosJugador++;
+            StartCoroutine(VictoriaIA());
         }
         else
         {
-            victoriasIA++;
+            puntosIA++;
+            StartCoroutine(RondaEmpate());
         }
     }
     
@@ -89,22 +98,66 @@ public class GameManager : MonoBehaviour
         // VAMOS A PARAR DE EJECUTAR LA FUNCIÓN
         // DURANTE 2.5 SEGUNDOS
         // UNA VEZ PASADOS, LA FUNCIÓN CONTINUARÁ EJECUTANDOSE
-        yield return new WaitForSeconds(2.5f);
-        
-
+        yield return new WaitForSeconds(0.5f);
 
         // 2 -- ANUNCIAR LA RONDA
         canvasTexto.text = "RONDA " + ronda;
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(0.5f);
 
 
         // 3 -- MOSTRAMOS LOS BOTONES
         canvasTexto.text = "ELIGE:";
         canvasPanel.SetActive(true);
+        //yield return null;
     }
 
     IEnumerator AvanzaRonda()
     {
+        canvasPanel.SetActive(false);
+        canvasTexto.text = "La IA está pensando...";
+        turno = 1;
+        yield return new WaitForSeconds(2.5f);
+    }
 
+    IEnumerator VictoriaJugador()
+    {
+        canvasPanel.SetActive(false);
+        canvasTexto.text = "¡Enhorabuena, ganaste esta ronda!";
+        yield return new WaitForSeconds(1.0f);
+    }
+
+    IEnumerator VictoriaIA()
+    {
+        canvasPanel.SetActive(false);
+        canvasTexto.text = "Qué lástima... La IA ganó esta ronda.";
+        yield return new WaitForSeconds(1.0f);
+    }
+
+    IEnumerator RondaEmpate()
+    {
+        canvasPanel.SetActive(false);
+        canvasTexto.text = "Empate";
+        yield return new WaitForSeconds(1.0f);
+    }
+
+    IEnumerator FinDeLaPartida()
+    {
+        canvasPanel.SetActive(false);
+        canvasTexto.text = "Estos son los resultados de la partida...";
+        yield return new WaitForSeconds(2.0f);
+
+        if (puntosJugador > puntosIA) // JUGADOR GANA
+        {
+            canvasTexto.text = "Has ganado.\n" + puntosJugador + " - " + puntosIA;
+        }
+        else if (puntosJugador < puntosIA) // IA GANA
+        {
+            canvasTexto.text = "Has perdido.\n" + puntosJugador + " - " + puntosIA;
+        }
+        else // EMPATE
+        {
+            canvasTexto.text = "Habéis empatado.\n" + puntosJugador + " - " + puntosIA;
+        }
+        yield return null;
     }
 }
