714f0aa70168e06f8783fc4e53281d98f8334770
santifont <sanalffer@alu.edu.gva.es>
2026-01-23 15:28:11 +0100

    Ajustes Iniciales
    Botones programados:
    - Opciones de Piedra, Papel, Tijera programados
    - Los botones han sido movido dentro del GameObject "Panel".
    ------
    - Falta establecer un número de rondas fijas y avanzar las rondas.

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -1,16 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CanvasManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameManager gameManagerScript;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void BotonPiedra()
+    {
+        gameManagerScript.TiradaJugador(0);
+    }
+
+    public void BotonPapel()
+    {
+        gameManagerScript.TiradaJugador(1);
+    }
+
+    public void BotonTijera()
+    {
+        gameManagerScript.TiradaJugador(2);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,39 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
-using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-    int turno = 0; // 0 -> Turno JUGADOR,  1 -> Turno IA
+    int turno = 0; // 0 --> JUGADOR
+                   // 1 --> IA
 
     // DATOS DE LA PARTIDA
-
     int ronda = 1;
     int victoriasJugador = 0;
-    int victoriasIA = 0;
     int empates = 0;
+    int victoriasIA = 0;
 
     // DATOS DE LA RONDA
     int tiradaJugador;
     int tiradaIA;
 
-    GameObject canvasPanel;
-    GameObject canvasTexto;
-    
+    GameObject      canvasPanel;
+    TextMeshProUGUI canvasTexto;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called before the first frame update
     void Start()
     {
         canvasPanel = GameObject.Find("PanelBotones");
-        canvasTexto = GameObject.Find("TextoPrincipal");
+        canvasTexto = GameObject.Find("TextoPrincipal").GetComponent<TextMeshProUGUI>();
 
+        //Bienvenida(); --> ERROR LLAMAR A LA CORRUTINA COMO UNA FUNCIÓN NORMAL
+        StartCoroutine(Bienvenida());
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (turno == 0) // TIRADA JUGADOR
+        {
+
+        }
+        else // TIRADA IA
+        {
+            TiradaIA();
+
+            //  CALCULAR QUIEN HA GANADO
+            CalculoGanador();
+            turno = 0;
+        }
+    }
+
+    // TIRADA JUGADOR:
+        // 0 -- piedra
+        // 1 -- papel
+        // 2 -- tijera
+    public void TiradaJugador(int tirada) 
+    {
+        tiradaJugador = tirada;
+        turno = 1;
+    }
+
+    public void TiradaIA()
+    {
+        tiradaIA = Random.Range(0, 3);
+    }
+
+    void CalculoGanador()
+    {
+       if (tiradaJugador == tiradaIA)
+        {
+            empates++;
+        }
+        else if (tiradaJugador == 0 & tiradaIA == 2 || tiradaJugador == 1 && tiradaIA == 0 || tiradaJugador == 2 && tiradaIA == 1)
+        {
+            victoriasJugador++;
+        }
+        else
+        {
+            victoriasIA++;
+        }
+    }
+    
+    IEnumerator Bienvenida()
+    {
+        // 1 -- BIENVENIDA AL JUEGO
+        canvasPanel.SetActive(false);
+        canvasTexto.text = "BIENVENIDO AL JUEGO PIEDRA-PAPEL-TIJERA";
+
+        // VAMOS A PARAR DE EJECUTAR LA FUNCIÓN
+        // DURANTE 2.5 SEGUNDOS
+        // UNA VEZ PASADOS, LA FUNCIÓN CONTINUARÁ EJECUTANDOSE
+        yield return new WaitForSeconds(2.5f);
         
+
+
+        // 2 -- ANUNCIAR LA RONDA
+        canvasTexto.text = "RONDA " + ronda;
+        yield return new WaitForSeconds(2.5f);
+
+
+        // 3 -- MOSTRAMOS LOS BOTONES
+        canvasTexto.text = "ELIGE:";
+        canvasPanel.SetActive(true);
+    }
+
+    IEnumerator AvanzaRonda()
+    {
+
     }
 }
